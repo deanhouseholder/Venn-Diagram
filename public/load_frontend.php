7d9b0201ffd3c0e0f703c2ee64eef48275735dcf
Dean Householder <deanhouseholder@gmail.com>
2022-10-12 04:47:08 +0000

    Updated ImageMap to v2.0.1

--- public/load_frontend.php
+++ public/load_frontend.php
@@ -2,26 +2,10 @@
 define('PP_DIR_PUBLIC', PP_DIR_BASE . 'public/');
 define('PP_DIR_PUBLIC_URL', PP_DIR_URL . 'public/');
 
-function pp_check_url() {
-    if (strpos($_SERVER['REQUEST_URI'], '/') !== false) {
-        wp_enqueue_script('jquery');
-        wp_enqueue_script('pp-main-script', PP_DIR_PUBLIC_URL . 'js/app.js');
-        wp_enqueue_script('pp-image-map-script', PP_DIR_PUBLIC_URL . 'js/image-map-1.0.8/image-map.min.js');
-        wp_enqueue_style('pp-main-style', PP_DIR_PUBLIC_URL . 'css/app.css');
-        add_filter('the_post', 'pp_venn_diagram');
-    }
+function pp_enqueue() {
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('pp-main-script', PP_DIR_PUBLIC_URL . 'js/app.js');
+    wp_enqueue_script('pp-image-map-script', PP_DIR_PUBLIC_URL . 'js/node_modules/image-map/dist/image-map.jquery.js');
+    wp_enqueue_style('pp-main-style', PP_DIR_PUBLIC_URL . 'css/app.css');
 }
-add_action('init', 'pp_check_url');
-
-function pp_venn_diagram($posts) {
-    //do all the stuff here
-
-    $posts = null;
-    $post = new stdClass();
-    $post->post_content = "Confirm Content";
-    $post->post_title = "Confirm";
-    $post->post_type = "page";
-    $post->comment_status = "closed";
-    $posts[] = $post;
-    return $posts . "hi";
-}
+add_action('wp_head', 'pp_enqueue');
